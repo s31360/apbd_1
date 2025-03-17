bc3c52576c34be6bbb8bc8dd13905e66bff7eb71
s31360 <s31360@pjwstk.edu.pl>
2025-03-17 15:58:48 +0100

    Added Find Avearge method

--- apbd_1APP/apbd_1APP/Program.cs
+++ apbd_1APP/apbd_1APP/Program.cs
@@ -7,11 +7,12 @@ class Program
     {
         int[] numbers = { 1, 2, 3, 4, 5 };
 
-        Console.WriteLine($"Max: {FindMax(numbers)}");
+        Console.WriteLine($"Average: {CalculateAverage(numbers)}");
     }
 
-    static int FindMax(int[] numbers)
+    static double CalculateAverage(int[] numbers)
     {
-        return numbers.Max();
+        if (numbers.Length == 0) return 0;
+        return numbers.Average();
     }
 }
